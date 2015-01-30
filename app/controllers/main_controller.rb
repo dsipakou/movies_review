2fc93e2dba174b9d1bc1c99347b02f8f2ef894e4
ror <ror@ror-virtual-machine.(none)>
2015-01-30 11:40:41 +0300

    number of movies sorter by comments fixed

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,10 +10,10 @@ class MainController < ApplicationController
 				@movies = Movie.order("created_at DESC").first(20)
 				@filter_title = "Последние добавленые"
 			when "2"
-				@movies = Movie.joins(:reviews).order('reviews.updated_at DESC').first(20).uniq()
+				@movies = Movie.joins(:reviews).order('reviews.updated_at DESC').first(100000).uniq().first(20)
 				@filter_title = "Последние рецензированые/оцененые"	
 			when "3"
-				@movies = Movie.joins(:comments).order('comments.updated_at DESC').first(20).uniq()
+				@movies = Movie.joins(:comments).order('comments.updated_at DESC').first(100000).uniq().first(20)
 				@filter_title = "Последние комментируемые"
 			when "4"
 				@movies = Movie.joins(:reviews).where.not(reviews: {content: nil}).group("reviews.movie_id").order("count(reviews.movie_id) DESC").first(20)
