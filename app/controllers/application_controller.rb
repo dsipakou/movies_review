c5f363dc697647d5cb3bd68b9bc6df2735f1e156
ror <ror@ror-virtual-machine.(none)>
2015-02-02 17:37:14 +0300

    misspelling fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  config.i18n.default_locale = :ru
+
   def current_user
   	return unless session[:userid]
   	@current_user ||= User.find(session[:userid])
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -13,13 +13,13 @@ class MainController < ApplicationController
 				@filter_title = "Последние добавленые"
 			when "2"
 				@movies = Movie.joins(:reviews).where("reviews.content <> '' OR reviews.stars <> '' OR reviews.awesome <> ''").order('reviews.updated_at DESC').first(100000).uniq().first(20)
-				@filter_title = "Последние рецензированые/оцененые"	
+				@filter_title = "Последние рецензированые/оцененные"	
 			when "3"
 				@movies = Movie.joins(:comments).order('comments.updated_at DESC').first(100000).uniq().first(20)
 				@filter_title = "Последние комментируемые"
 			when "4"
 				@movies = Movie.joins(:reviews).where.not(reviews: {content: nil}).group("reviews.movie_id").order("count(reviews.movie_id) DESC").first(20)
-				@filter_title = "Самые рецинзируемые"
+				@filter_title = "Самые рецензируемые"
 			when "5"
 				@movies = Movie.joins(:reviews).where(reviews: {awesome: 1}).group("reviews.movie_id").order("count(reviews.movie_id) DESC").first(20)
 				@filter_title = "Сначала самые охуенные"
