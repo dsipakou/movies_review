548e30cb9a50f91c7c2a2a614b4fa7be6639c5e0
ror <ror@ror-virtual-machine.(none)>
2015-02-25 19:02:56 +0300

    post editing added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,16 +9,20 @@ class PostsController < ApplicationController
 		@posts = Post.order("created_at DESC")
 	end
 
+  def edit
+    authorize! :edit, @users
+  end
+
 	def new
     	authorize! :new, @post
     	@post = Post.new
-  	end
+  end
 
-  	def create
+  def create
     @post = Post.new(post_params)
     respond_to do |format|
       if @post.save
-        format.html { redirect_to posts_path, notice: 'Post was successfully created.' }
+        format.html { redirect_to posts_path, notice: 'Ура! Пост готов и слава БОГУ!' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -27,16 +31,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to posts_path, notice: 'Обновились.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
 	private
 		def set_post
 			@post = Post.find(params[:id])
 		end
 
 		def post_params
-      		params.require(:post).permit(:user_id, :content)
-    	end
+    	params.require(:post).permit(:user_id, :content)
+    end
 
-    	def modify_content
-		      params[:post][:content] = params[:post][:content].gsub(/(\r)?\n/, '<br />').gsub('<img', '<br /><img').gsub('/img>', '/img><br />') unless params[:post][:content].nil?
-	    end
+    def modify_content
+		  params[:post][:content] = params[:post][:content].gsub(/(\r)?\n/, '<br />').gsub('<img', '<br /><img').gsub('/img>', '/img><br />') unless params[:post][:content].nil?
+	  end
 end
